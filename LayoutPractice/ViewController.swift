04a4891c97e7d598870fa4e50dde82427ca780d6
Hanyu.chen <rollr76518@gmail.com>
2020-03-18 22:26:41 +0800

    Initial Commit

---
+++ LayoutPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LayoutPractice
+//
+//  Created by Ryan on 2020/3/18.
+//  Copyright © 2020 Hanyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
